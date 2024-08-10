05cc4f268a60313fdf2cf7ccd90756511d20f2eb
sinke237 <enowsinke@gmail.com>
2024-08-10 17:47:19 +0100

    feat(integration): connect backend to database

--- backend-readwell/src/main/java/org/prominds/backendReadwell/BackendReadwellApplication.java
+++ backend-readwell/src/main/java/org/prominds/backendReadwell/BackendReadwellApplication.java
@@ -2,8 +2,10 @@ package org.prominds.backendReadwell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.prominds.backendReadwell", "controller"})
 public class BackendReadwellApplication {
 
 	public static void main(String[] args) {
