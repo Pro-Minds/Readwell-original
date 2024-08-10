8fa588c221ae217b1029a4b8a2c8e2ac2db3e863
sinke237 <enowsinke@gmail.com>
2024-08-10 10:56:18 +0100

    refactor(backend): init backend module

---
+++ backend-readwell/src/test/java/org/prominds/backendReadwell/BackendReadwellApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prominds.backendReadwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendReadwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
