d35fdbd75ecf56b5381b5146ce16dee15546d86d
sinke237 <enowsinke@gmail.com>
2024-08-11 14:57:41 +0100

    refactor(integration): testing that request can live fend to db and db to fend via bEnd

--- backend-readwell/src/main/java/controller/HelloController.java
+++ backend-readwell/src/main/java/controller/HelloController.java
@@ -3,13 +3,20 @@ package controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @CrossOrigin
     @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello World from Spring Boot!";
+    public List<String> hello() {
+        return jdbcTemplate.query("SELECT message FROM test_data", (rs, rowNum) -> rs.getString("message"));
     }
 }
