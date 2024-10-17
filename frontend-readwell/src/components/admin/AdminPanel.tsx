51c73b76b5eed4482e0a101f7d77572f74645607
sinke237 <enowsinke@gmail.com>
2024-10-17 07:46:03 +0100

    refactor: styling admin page

--- frontend-readwell/src/components/admin/AdminPanel.tsx
+++ frontend-readwell/src/components/admin/AdminPanel.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { logout } from '../../security/AuthService';
 import {Link} from "react-router-dom";
-
+import styles from '../../components/AdminStyles.module.css';
 const AdminPanel = () => {
 
     const handleLogout = () => {
@@ -9,12 +9,12 @@ const AdminPanel = () => {
     };
 
     return (
-        <div>
-            <h2>Welcome to the Admin Panel</h2>
-            <Link to="/admin/manage-klasses">Manage Klasses</Link><br/>
-            <Link to="/admin/manage-subjects">Manage Subjects</Link><br/>
-            <Link to="/admin/manage-topics">Manage Topics</Link><br/>
-            <Link to="/admin/manage-questions">Manage Questions</Link><br/>
+        <div className={styles.adminUI}>
+            <h2 className={styles.head}>Welcome to the Admin Panel</h2>
+            <Link className={styles.link} to="/admin/manage-klasses">Manage Klasses</Link><br/>
+            <Link className={styles.link} to="/admin/manage-subjects">Manage Subjects</Link><br/>
+            <Link className={styles.link} to="/admin/manage-topics">Manage Topics</Link><br/>
+            <Link className={styles.link} to="/admin/manage-questions">Manage Questions</Link><br/>
             <button onClick={handleLogout}>Logout</button>
         </div>
     );
