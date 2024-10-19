8c8521a0764d61410b0cf320457b6837296217e1
sinke237 <enowsinke@gmail.com>
2024-10-19 11:42:18 +0100

    refactor: styling user homepage

--- frontend-readwell/src/components/user/HomePage.tsx
+++ frontend-readwell/src/components/user/HomePage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ClassList from "./ClassList"; // Import ClassList to display classes
-
+import styles from '../HomePageStyles.module.css';
 const HomePage: React.FC = () => {
     return (
-        <div>
+        <div className={styles.home}>
             <h1>Welcome to the Home Page!</h1>
             <ClassList /> {/* Render the ClassList component */}
         </div>
