cef62de2768f8b5df696427badaa0607b27e28e7
sinke237 <enowsinke@gmail.com>
2024-10-20 09:52:40 +0100

    refactor: removed dotenv dependency

--- backend-readwell/src/main/java/org/prominds/backendReadwell/BackendReadwellApplication.java
+++ backend-readwell/src/main/java/org/prominds/backendReadwell/BackendReadwellApplication.java
@@ -1,6 +1,5 @@
 package org.prominds.backendReadwell;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -22,13 +21,6 @@ public class BackendReadwellApplication implements CommandLineRunner {
 	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // Load .env file
-
-		// Set system properties from .env
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
-
 		SpringApplication.run(BackendReadwellApplication.class, args);
 	}
 
